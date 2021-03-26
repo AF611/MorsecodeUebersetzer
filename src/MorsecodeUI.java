6799be32aa7fa41eb8de7b12b833adf419a7ed34
AF611 <alex2000.fritz@web.de>
2021-03-26 21:47:47 +0100

    Methode Morsezeichen Zuweisen

--- src/MorsecodeUI.java
+++ src/MorsecodeUI.java
@@ -6,11 +6,11 @@ import java.awt.event.ActionListener;
 public class MorsecodeUI {
     private JPanel jpanel;
     private JLabel jLabelÜberschrift;
-    private JTextField textFieldEingabe;
     private JTextField textFieldAusgabe;
     private JButton übersetzenButton;
     private JLabel jLabelText;
     private JLabel jLabelAusgabe;
+    private JTextField textFieldEingabe;
 
     public MorsecodeUI(){
 
@@ -18,9 +18,26 @@ public class MorsecodeUI {
         übersetzenButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.out.println("Übersetzt");
+
+                morseZeichenZuweisen();
             }
         });
+    }
+
+    public void morseZeichenZuweisen(){
+        String[] morsezeichen = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-",
+                ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.", "--.."};
+
+        String[] alphabet = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
+                "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+
+        String str = textFieldEingabe.getText();
+
+        for(int i = 0; i < str.length(); i++){
+
+        }
+        morsezeichen [1] = alphabet[1];
+        textFieldAusgabe.setText("Hallo");
     }
 
 
